77b5c237a0541de687ea7e703eab04f4cd292836
XFFXFF <fan.zhou@watrix.ai>
2019-08-03 16:06:32 +0800

    less model

--- model_speed_test.py
+++ model_speed_test.py
@@ -3,7 +3,12 @@ import time
 from thop import profile, clever_format
 
 
-model_names = torch.hub.list("pytorch/vision")
+# model_names = torch.hub.list("pytorch/vision")
+model_names = ['alexnet', 'densenet121', 'densenet161', 'densenet169', 'densenet201', 
+                'googlenet', 'inception_v3', 'mobilenet_v2', 'resnet101', 'resnet152',
+                'resnet18', 'resnet34', 'resnet50', 'resnext101_32x8d', 'resnext50_32x4d', 
+                'shufflenet_v2_x0_5', 'shufflenet_v2_x1_0', 'squeezenet1_0', 'squeezenet1_1', 
+                'vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn', 'vgg16', 'vgg16_bn', 'vgg19', 'vgg19_bn']
 with open("model_spped.txt", "w") as f:
     for model_name in model_names:
         model = torch.hub.load('pytorch/vision', model_name, pretrained=False)
